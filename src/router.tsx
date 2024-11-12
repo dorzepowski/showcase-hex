fe08ac34f4e6dfa1ec521226ba5992c5be009135
Damian Orzepowski <damian.orzepowski@softwarelab.com.pl>
2024-11-12 17:00:29 +0100

    feat: handle base name

--- src/router.tsx
+++ src/router.tsx
@@ -7,49 +7,52 @@ import { RawTxView } from './pages/RawTxView/RawTxView.tsx';
 import BUMPApp from './pages/BUMPApp/BUMPApp.tsx';
 import { BUMPView } from './pages/BUMPView/BUMPView.tsx';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Navigate to="/beef" />,
-      },
-      {
-        path: 'beef/',
-        element: <BEEFApp />,
-        children: [
-          {
-            path: ':beef/',
-            element: <BEEFView />,
-          },
-        ],
-      },
-      {
-        path: 'rawtx/',
-        element: <RawTxApp />,
-        children: [
-          {
-            path: ':rawtx/',
-            element: <RawTxView />,
-          },
-        ],
-      },
-      {
-        path: 'bump/',
-        element: <BUMPApp />,
-        children: [
-          {
-            path: ':bump/',
-            element: <BUMPView />,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: '*',
-    element: <Navigate to="/" />,
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Navigate to="/beef" />,
+        },
+        {
+          path: 'beef/',
+          element: <BEEFApp />,
+          children: [
+            {
+              path: ':beef/',
+              element: <BEEFView />,
+            },
+          ],
+        },
+        {
+          path: 'rawtx/',
+          element: <RawTxApp />,
+          children: [
+            {
+              path: ':rawtx/',
+              element: <RawTxView />,
+            },
+          ],
+        },
+        {
+          path: 'bump/',
+          element: <BUMPApp />,
+          children: [
+            {
+              path: ':bump/',
+              element: <BUMPView />,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: '*',
+      element: <Navigate to="/" />,
+    },
+  ],
+  { basename: import.meta.env.BASE_URL },
+);
