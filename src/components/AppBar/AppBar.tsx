db7234cd323b9534a2ab787b3dab3729d2f19884
Damian Orzepowski <damian.orzepowski@softwarelab.com.pl>
2024-11-13 13:21:19 +0100

    fix: page layout

--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -2,7 +2,7 @@ import { AppBar, Toolbar, Typography } from '@mui/material';
 
 export const TopBar = () => {
   return (
-    <AppBar sx={{ marginBottom: '1rem' }}>
+    <AppBar sx={{ marginBottom: '1rem' }} position='relative'>
       <Toolbar>
         <Typography variant="h6" component="div">
           Background Evaluation Extended Format (BEEF)
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,19 +1,16 @@
 import { TopBar } from '../../components/AppBar/AppBar.tsx';
-import { Box, Grid2, Stack } from '@mui/material';
+import { Grid2, Stack } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
 export const Layout = () => {
   return (
-    <>
-      <TopBar />
-      <Grid2 container>
-        <Stack sx={{ width: '100vw', height: '100vh' }}>
-          <Box sx={{ marginBottom: '6rem' }} />
-          <Grid2 size={12}>
-            <Outlet />
-          </Grid2>
-        </Stack>
-      </Grid2>
-    </>
+    <Grid2 container>
+      <Stack sx={{ width: '100vw', height: '100dvh' }}>
+        <TopBar />
+        <Grid2 size={12}>
+          <Outlet />
+        </Grid2>
+      </Stack>
+    </Grid2>
   );
 };
